126f86d1b39a405998f16064efff97a5a5ffc9c7
Field Coordinator <coordin@Macintosh.local>
2016-11-14 22:07:24 -0500

    added facet grid to figure 16

--- scripts/galton/figure16.R
+++ scripts/galton/figure16.R
@@ -47,12 +47,15 @@ trainZeroNotZeroBG <- ggplot(aucVals, aes(x=V1, y=V2)) +
         axis.title.y = element_text(size=36),
         text = element_text(size=30)) +
   coord_cartesian(ylim=c(.6,1)) +
-  ggtitle("Training", size=20) + 
+  ggtitle("Training") +
   geom_hline(yintercept=.94, linetype="longdash", colour="black", size=0.5) + 
   xlab("Sequence") +
   ylab("AUC") +
   theme_bw()
 
+aucVals$V5 <- rep('0 vs !0', 4)
+aucVals$V6 <- rep('Training', 4)
+aucValsAll <- aucVals
 outputVal <- all.valid.data$averageRating.x
 w <- 1
 aucVals <- NULL
@@ -73,13 +76,15 @@ validZeroNotZeroBG <- ggplot(aucVals, aes(x=V1, y=as.numeric(as.character(V2))))
         axis.title.y = element_text(size=36),
         text = element_text(size=30)) +
   coord_cartesian(ylim=c(.6,1)) +
-  ggtitle("Validation", size=20) + 
+  ggtitle("Validation") +
   geom_hline(yintercept=.96, linetype="longdash", colour="black", size=0.5) + 
   xlab("Sequence") +
   ylab("AUC") + 
   theme_bw()
 
-
+aucVals$V5 <- rep('0 vs !0', 4)
+aucVals$V6 <- rep('Validation', 4)
+aucValsAll <- rbind(aucValsAll, aucVals)
 
 
 # Now do the 1 vs 2 data 
@@ -89,7 +94,8 @@ raw.lme.data$averageRating.x <- as.numeric(as.character(raw.lme.data$averageRati
 raw.lme.data <- raw.lme.data[which(raw.lme.data$averageRating.x!=0),]
 raw.lme.data$averageRating.x[raw.lme.data$averageRating.x<1.5] <- 1
 raw.lme.data$averageRating.x[raw.lme.data$averageRating.x>1.5] <- 2
-folds <- createFolds(raw.lme.data$averageRating.x, k=3, list=T, returnTrain=T)
+#folds <- createFolds(raw.lme.data$averageRating.x, k=3, list=T, returnTrain=T)
+load('/home/adrose/qapQA/data/foldsToUse1vs2.RData')
 raw.lme.data[,2:32] <- scale(raw.lme.data[,2:32], center=T, scale=T)
 index <- unlist(folds[1])
 trainingData <- raw.lme.data[index,]
@@ -127,6 +133,9 @@ trainOneVsTwoBG <- ggplot(aucVals, aes(x=V1, y=as.numeric(as.character(V2)))) +
   theme_bw()
 
 
+aucVals$V5 <- rep('1 vs 2', 4)
+aucVals$V6 <- rep('Training', 4)
+aucValsAll <- rbind(aucValsAll, aucVals)
 
 # Now do the 1 vs 2 validation data
 outputVal <- all.valid.data$averageRating.x
@@ -155,8 +164,33 @@ validOneVsTwoBG <- ggplot(aucVals, aes(x=V1, y=as.numeric(as.character(V2)))) +
   ylab("AUC") + 
   theme_bw()
 
+aucVals$V5 <- rep('1 vs 2', 4)
+aucVals$V6 <- rep('Validation', 4)
+aucValsAll <- rbind(aucValsAll, aucVals)
 
-# Now plot all of this stuff 
+horizontalDataFrame <- as.data.frame(cbind(c('0 vs !0', '0 vs !0', '1 vs 2', '1 vs 2'),
+                             c('Training', 'Validation', 'Training', 'Validation'),
+                             c(.94, .96, .87, .78)))
+colnames(horizontalDataFrame) <- c('V5', 'V6', 'V7')
+horizontalDataFrame$V7 <- as.numeric(as.character(horizontalDataFrame$V7))
+
+thing1 <- ggplot(aucValsAll, aes(x=V1, y=as.numeric(as.character(V2)))) +
+  geom_bar(stat="identity", width=0.4, position=position_dodge(width=0.5)) +
+  theme_bw() +
+  theme(legend.position="none",
+  axis.text.x = element_text(angle=90,hjust=1, size=26, face="bold"),
+  axis.text.y = element_text(size=26, face="bold"),
+  axis.title=element_text(size=36,face="bold"),
+  strip.text.y = element_text(size = 30, angle = 270, face="bold"),
+  strip.text.x = element_text(size = 30, angle = 0, face="bold")) +
+  coord_cartesian(ylim=c(.6,1)) +
+  geom_hline(data=horizontalDataFrame, aes(yintercept=V7), linetype="longdash", colour="black", size=0.5) +
+  xlab("Sequence") +
+  ylab("AUC") +
+  facet_grid(V5 ~ V6)
+
+
+# Now plot all of this stuff
 pdf('figure16-motionAsIdentifierAUCValues.pdf', height=20, width=20)
-multiplot(trainZeroNotZeroBG, trainOneVsTwoBG, validZeroNotZeroBG, validOneVsTwoBG, cols=2)
+thing1
 dev.off()
