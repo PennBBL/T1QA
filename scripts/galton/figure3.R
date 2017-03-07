cd42369752f06bc8b962b7b2446839ffbd1d5482
Adon Rosen <adrose@galton.uphs.upenn.edu>
2017-03-07 16:00:29 -0500

    Updated with spearmans rho, wilcox's p value

--- scripts/galton/figure3.R
+++ scripts/galton/figure3.R
@@ -10,7 +10,7 @@ source('/home/adrose/T1QA/scripts/galton/loadGo1Data.R')
 set.seed(16)
 
 # load library(s)
-install_load('caret', 'ggplot2', 'lme4', 'car', 'visreg')
+install_load('caret', 'ggplot2', 'lme4', 'car', 'visreg', 'scales', 'MASS')
 
 # Now lets create our train and validation sets
 raw.lme.data <- merge(isolatedVars, manualQAData2, by='bblid')
@@ -44,12 +44,14 @@ bg2.vals$Dataset <- factor(bg2.vals$Dataset)
 bg2.vals$sex <- c('Male', 'Female', 'Male', 'Female')
 
 # Now lets plot our values
+# Grab a p value from a t.test
+pValue <- t.test(all.train.data$averageRating ~ all.train.data$sex)
+pValue <- wilcox.test(all.train.data$averageRating ~ all.train.data$sex)
 bg1 <- ggplot(bg2.vals[which(bg2.vals$Dataset=='Training'),], aes(x=factor(sex), y=as.numeric(as.character(averageRating)), group=Dataset)) + 
-                geom_bar(stat='identity', position=position_dodge(), size=.1, aes(fill=Dataset)) + 
+                geom_bar(stat='identity', position=position_dodge(), width=.5) + 
                 labs(title='Training', x='Sex', y='Manual Quality Rating (mean value)') +
                 theme_bw() + 
-                coord_cartesian(ylim=c(1.6,2)) + 
-                geom_bar(stat="identity", position=position_dodge(), size=.1) + 
+                coord_cartesian(ylim=c(1.6,2.1)) + 
                        geom_errorbar(aes(ymin=as.numeric(as.character(averageRating))-se, 
                                          ymax=as.numeric(as.character(averageRating))+se), 
                 width = .1, position=position_dodge(.9)) +
@@ -58,14 +60,22 @@ bg1 <- ggplot(bg2.vals[which(bg2.vals$Dataset=='Training'),], aes(x=factor(sex),
                 axis.text=element_text(size=20),
                 axis.title=element_text(size=30),
                 strip.text.y = element_text(size = 16, angle = 270, face="bold"),
-                title=element_text(size=30))
+                title=element_text(size=30)) + 
+		geom_path(aes(x=factor(sex), y=c(2,2))) +
+		geom_path(aes(x=factor(sex)[1], y=c(1.9, 2))) +
+		geom_path(aes(x=factor(sex)[2], y=c(1.95, 2))) +
+		geom_text(aes(x=factor(sex)[1], y=2.05), label='*',angle=90, size=10) +
+		scale_y_continuous(limits=c(1.6, 2.1), 
+                           breaks=round(seq(1.6, 2.1, .2), digits=2), oob=rescale_none) + 
+		annotate("text", x=c(Inf), y=c(Inf), label="p < 0.05", hjust=c(2.1), vjust=c(1.1), size=8, parse=T)
+		
 
+pValue <- t.test(all.valid.data$averageRating ~ all.valid.data$sex)
+pValue <- wilcox.test(all.valid.data$averageRating ~ all.valid.data$sex)
 bg2 <- ggplot(bg2.vals[which(bg2.vals$Dataset=='Validation'),], aes(x=factor(sex), y=as.numeric(as.character(averageRating)), group=Dataset)) +
-                geom_bar(stat='identity', position=position_dodge(), size=.1, aes(fill=Dataset)) + 
+                geom_bar(stat='identity', position=position_dodge(), width=.5) + 
                 labs(title='Validation', x='Sex', y='Manual Quality Rating (mean value)') +
                 theme_bw() + 
-                coord_cartesian(ylim=c(1.6,2)) + 
-                geom_bar(stat="identity", position=position_dodge(), size=.1) + 
                        geom_errorbar(aes(ymin=as.numeric(as.character(averageRating))-se, 
                                          ymax=as.numeric(as.character(averageRating))+se), 
                 width = .1, position=position_dodge(.9)) +
@@ -77,9 +87,21 @@ bg2 <- ggplot(bg2.vals[which(bg2.vals$Dataset=='Validation'),], aes(x=factor(sex
                 axis.title=element_text(size=30),
 		axis.ticks.y=element_blank(),
                 strip.text.y = element_text(size = 16, angle = 270, face="bold"),
-                title=element_text(size=30))
+                title=element_text(size=30)) + 
+		geom_path(aes(x=factor(sex), y=c(2,2))) +
+		geom_path(aes(x=factor(sex)[1], y=c(1.85, 2))) +
+		geom_path(aes(x=factor(sex)[2], y=c(1.95, 2))) +
+		geom_text(aes(x=factor(sex)[1], y=2.05), label='***',angle=90, size=10) +
+		scale_y_continuous(limits=c(1.6, 2.1), 
+                           breaks=round(seq(1.6, 2.1, .2), digits=2), oob=rescale_none) + 
+		annotate("text", x=c(Inf), y=c(Inf), label="p < 0.001", hjust=c(2), vjust=c(1.1), size=8, parse=T)		
+
 
 # Now build our models to show geneerl age trends
+corVal <- cor(all.train.data$rawAverageRating.x, all.train.data$age, method='spearman')
+corSig <- cor.test(all.train.data$rawAverageRating.x, all.train.data$age, method='spearman')$p.value
+corText1 <- expression("Spearman's"~rho == .15)
+corText2 <- paste("p < 0.01")
 mod1 <- ggplot(all.train.data, aes(x=rawAverageRating.x, y=age)) +
    geom_smooth(method=lm, color='black') +
    theme_bw() +
@@ -88,8 +110,14 @@ mod1 <- ggplot(all.train.data, aes(x=rawAverageRating.x, y=age)) +
    theme(
     axis.text=element_text(size=20),
     axis.title=element_text(size=30)) +
-   scale_x_continuous(breaks=c(0,.33,.66,1,1.33,1.66,2))
+   scale_x_continuous(breaks=c(0,.33,.66,1,1.33,1.66,2)) + 
+   annotate("text", x=c(Inf, Inf), y=c(-Inf, -Inf), label=c(as.character(corText2), as.character(corText1)), hjust=c(1, 1), vjust=c(-.5, -2.5), size=8, parse=T)
 
+
+corVal <- cor(all.valid.data$rawAverageRating.x, all.valid.data$age, method='spearman')
+corSig <- cor.test(all.valid.data$rawAverageRating.x, all.valid.data$age, method='spearman')$p.value
+corText1 <- expression("Spearman's"~rho == paste(0.31))
+corText2 <- paste("p < 0.01")
 mod2 <- ggplot(all.valid.data, aes(x=rawAverageRating.x, y=age)) +
    geom_smooth(method=lm, color='black') +
    theme_bw() +
@@ -101,7 +129,8 @@ mod2 <- ggplot(all.valid.data, aes(x=rawAverageRating.x, y=age)) +
     axis.title.y=element_text(size=20, color='white'),
     axis.text.y=element_text(size=30, color='white'),
     axis.ticks.y=element_blank()) +
-    scale_x_continuous(breaks=c(0,.33,.66,1,1.33,1.66,2))
+    scale_x_continuous(breaks=c(0,.33,.66,1,1.33,1.66,2)) + 
+   annotate("text", x=c(Inf, Inf), y=c(-Inf, -Inf), label=c(as.character(corText2), as.character(corText1)), hjust=c(1, 1), vjust=c(-.5, -2.5), size=8, parse=T)
 
 png('figure3-demographicsvsRatingQAPPaper.png', width=16, height=16, units='in', res=300)
 multiplot(bg1, mod1, bg2, mod2, cols=2)
