505bd15aea3276fe1c2a0c2ca83bddb6f3f3fb16
Adon Rosen <arose@Adons-MacBook-Pro.local>
2017-05-14 18:09:35 -0400

    Added rough scripts for revisions R1

---
+++ scripts/galton/revisionsR1/computeROIWiseEffectSize-UNIVARIATE-FS.R
@@ -0,0 +1,121 @@
+# AFGR October 24 2016
+# This script is going to be used to compute the effect size of the outcome of the 1 vs 2 model on individual ROI values
+# cortical thickness and volume values will be being predicted as well as GMD just for the heck of it.
+# This will be done in both hemispheres which is also important to note.
+# The various steps involved in this process are:
+#	0.) Produce age regressed values : may or may not include this
+#	1.) Model our individual ROI following this method: lm(individualROI ~ qapValues)
+#	2.) Perform FDR correction on all ROI's
+
+
+## Load the data
+source('/home/adrose/T1QA/scripts/galton/loadGo1Data.R')
+detachAllPackages()
+set.seed(16)
+load('/home/adrose/qapQA/data/1vs28variableModel.RData')
+oneVsTwoModel <- mod8
+rm(mod8)
+tbvData <- read.csv('/home/adrose/dataPrepForHiLoPaper/data/preRaw/t1/n1601_antsCtVol.csv')
+## Now load all of the freesurfer values
+fsVol <- read.csv('/home/adrose/qapQA/data/n1601_freesurferVol_20161220.csv')
+fsCt <- read.csv('/home/adrose/qapQA/data/n1601_freesurferCt_20161220.csv')
+fsVals <- merge(fsVol, fsCt, by=c('bblid', 'scanid'))
+## Source all functions
+source('/home/adrose/T1QA/scripts/galton/computeROIWiseEffectSizeFucntions.R')
+
+## Load library(s) we will need
+install_load('caret', 'lme4', 'bda', 'ggplot2')
+
+## Now lets prep the data
+## Now create the training data set and create the outcomes for all of the training data sets
+raw.lme.data <- merge(isolatedVars, manualQAData2, by='bblid')
+raw.lme.data <- merge(raw.lme.data, tbvData, by='bblid')
+raw.lme.data$averageRating.x <- as.numeric(as.character(raw.lme.data$averageRating.x))
+raw.lme.data$averageRating.x[raw.lme.data$averageRating.x>1] <- 1
+#folds <- createFolds(raw.lme.data$averageRating.x, k=3, list=T, returnTrain=T)
+load('/home/adrose/qapQA/data/foldsToUse.RData')
+raw.lme.data[,3:32] <- scale(raw.lme.data[,3:32], center=T, scale=T)
+index <- unlist(folds[1])
+trainingData <- raw.lme.data[index,]
+validationData <- raw.lme.data[-index,]
+## Now create our outcomes
+# First create our zero vs not zero outcome for everyone
+trainingData$variable <- rep('ratingNULL', nrow(trainingData))
+validationData$variable <- rep('ratingNULL', nrow(validationData))
+# Now lets do our 1 vs 2 model for everyone
+trainingData$oneVsTwoOutcome <- predict(oneVsTwoModel, newdata=trainingData,
+allow.new.levels=T, type='response')
+validationData$oneVsTwoOutcome <- predict(oneVsTwoModel, newdata=validationData,
+allow.new.levels=T, type='response')
+## Now merge our scaled data values with the original data values
+all.train.data <- merge(mergedQAP, trainingData, by='bblid')
+all.train.data <- merge(all.train.data, fsVals, by='bblid')
+all.valid.data <- merge(mergedQAP, validationData, by='bblid')
+all.valid.data <- merge(all.valid.data, fsVals, by='bblid')
+
+# Remove 0 scans 
+all.train.data <- all.train.data[which(all.train.data$averageRating!=0),]
+all.valid.data <- all.valid.data[which(all.valid.data$averageRating!=0),]
+
+
+# Now create our z scores
+tmp <- all.train.data[,-seq(2862, 2997)[1:38]]
+fsCTVals <- pvalLoop('_thickness', tmp)
+rm(tmp)
+# Now trim the non cortical regions for our JLF vol regions
+tmp <- all.train.data
+all.train.data <- all.train.data[,-seq(2592,2627,1)]
+fsVOLVals <- pvalLoop('_volume', all.train.data, TBV=TRUE)
+all.train.data <- tmp
+rm(tmp)
+
+## Now create our color values to export to ITK snap
+ctColors <- returnPosNegAndNeuColorScale(jlfCTVals[,2], colorScaleNeg=c('blue', 'light blue'),colorScalePos=c('yellow', 'red'))
+gmdColors <- returnPosNegAndNeuColorScale(jlfGMDVals[,2], colorScaleNeg=c('blue', 'light blue'), colorScalePos=c('yellow', 'red'))
+volColors <- returnPosNegAndNeuColorScale(jlfVOLVals[,2], colorScaleNeg=c('blue', 'light blue'), colorScalePos=c('yellow', 'red'))
+
+# Now we need to create our label into our file which matches our ROI to our label
+fsCTVals <- cbind(jlfCTVals, ctColors[2:(dim(jlfCTVals)[1]+1),1])
+fsVOLVals <- cbind(jlfVOLVals, volColors[2:(dim(jlfVOLVals)[1]+1),1])
+
+# Now I need to save these color scales and the other thing
+write.table(ctColors, file='ctColorScale.txt', sep="\t", quote=F, row.names=F, col.names=F)
+write.table(gmdColors, file='gmdColorScale.txt', sep="\t", quote=F, row.names=F, col.names=F)
+write.table(volColors, file='volColorScale.txt', sep="\t", quote=F, row.names=F, col.names=F)
+write.csv(jlfCTVals, 'jlfSigQAPROIct.csv', quote=F)
+write.csv(jlfGMDVals, 'jlfSigQAPROIgmd.csv', quote=F)
+write.csv(jlfVOLVals, 'jlfSigQAPROIvol.csv', quote=F)
+
+# Now do the validation data down here
+static <- all.train.data
+all.train.data <- all.valid.data
+
+tmp <- all.train.data[,-seq(2862, 2997)[1:38]]
+fsCTVals <- pvalLoop('_thickness', tmp)
+rm(tmp)
+# Now trim the non cortical regions for our JLF vol regions
+tmp <- all.train.data
+all.train.data <- all.train.data[,-seq(2592,2627,1)]
+jlfVOLVals <- pvalLoop('_volume', all.train.data, TBV=TRUE)
+all.train.data <- tmp
+rm(tmp)
+
+## Now create our color values to export to ITK snap
+ctColors <- returnPosNegAndNeuColorScale(jlfCTVals[,2], colorScaleNeg=c('blue', 'light blue'), colorScalePos=c('yellow', 'red'))
+gmdColors <- returnPosNegAndNeuColorScale(jlfGMDVals[,2], colorScaleNeg=c('blue', 'light blue'), colorScalePos=c('yellow', 'red'))
+volColors <- returnPosNegAndNeuColorScale(jlfVOLVals[,2], colorScaleNeg=c('blue', 'light blue'), colorScalePos=c('yellow', 'red'))
+
+# Now we need to create our label into our file which matches our ROI to our label
+jlfCTVals <- cbind(jlfCTVals, ctColors[2:(dim(jlfCTVals)[1]+1),1])
+jlfGMDVals <- cbind(jlfGMDVals, gmdColors[2:(dim(jlfGMDVals)[1]+1),1])
+jlfVOLVals <- cbind(jlfVOLVals, volColors[2:(dim(jlfVOLVals)[1]+1),1])
+
+
+# Now I need to save these color scales and the other thing
+write.table(ctColors, file='ctColorScaleValid.txt', sep="\t", quote=F, row.names=F, col.names=F)
+write.table(gmdColors, file='gmdColorScaleValid.txt', sep="\t", quote=F, row.names=F, col.names=F)
+write.table(volColors, file='volColorScaleValid.txt', sep="\t", quote=F, row.names=F, col.names=F)
+write.csv(jlfCTVals, 'jlfSigQAPROIctValid.csv', quote=F)
+write.csv(jlfGMDVals, 'jlfSigQAPROIgmdValid.csv', quote=F)
+write.csv(jlfVOLVals, 'jlfSigQAPROIvolValid.csv', quote=F)
+
---
+++ scripts/galton/revisionsR1/figure3_yHat.R
@@ -0,0 +1,277 @@
+# AFGR June 6 2016
+# This script is going to be used
+# to showcase the differences of age amongst our 
+# rating metrics for the qap paper
+# across the training and testing data sets
+
+## Load data
+# Start with Go1
+source('/home/adrose/T1QA/scripts/galton/loadGo1Data.R')
+set.seed(16)
+load('/home/adrose/qapQA/data/0vsNot0FinalData.RData')
+zeroVsNotZeroModel <- m1
+rm(m1)
+load('/home/adrose/qapQA/data/1vs28variableModel.RData')
+oneVsTwoModel <- mod8
+rm(mod8)
+# load library(s)
+install_load('caret', 'ggplot2', 'lme4', 'car', 'visreg', 'scales', 'MASS')
+
+# Now lets create our train and validation sets
+raw.lme.data <- merge(isolatedVars, manualQAData2, by='bblid')
+raw.lme.data$averageRating.x <- as.numeric(as.character(raw.lme.data$averageRating.x))
+raw.lme.data$averageRating.x[raw.lme.data$averageRating.x>1] <- 1
+raw.lme.data[,2:32] <- scale(raw.lme.data[,2:32], center=T, scale=T)
+load('/home/adrose/qapQA/data/foldsToUse.RData')
+index <- unlist(folds[1])
+trainingData <- raw.lme.data[index,]
+validationData <- raw.lme.data[-index,] 
+manualQAData$age <- (manualQAData$ageAtGo1Scan / 12)
+
+# Now create our model outcome column for both 0Vs !0 & 1 vs 2 in both data sets
+trainingData$variable <- 'ratingNULL'
+validationData$variable <- 'ratingNULL'
+trainingData$oneVsTwoOutcome <- predict(oneVsTwoModel, newdata=trainingData,allow.new.levels=T, type='response')
+trainingData$zeroVsNotZeroOutcome <- predict(zeroVsNotZeroModel, newdata=trainingData, allow.new.levels=T, type='response')
+validationData$oneVsTwoOutcome <- predict(oneVsTwoModel, newdata=validationData, allow.new.levels=T, type='response')
+validationData$zeroVsNotZeroOutcome <- predict(zeroVsNotZeroModel, newdata=validationData, allow.new.levels=T, type='response')
+
+# Now prep our individual data sets
+all.train.data <- merge(trainingData, manualQAData, by='bblid')
+all.valid.data <- merge(validationData, manualQAData, by='bblid')
+
+# Now prepare our values
+bg2.vals.train <- summarySE(data=all.train.data, measurevar='oneVsTwoOutcome', groupvars='sex')
+bg2.vals.train$Dataset <- rep('Training', nrow(bg2.vals.train))
+bg2.vals.valid <- summarySE(data=all.valid.data, measurevar='oneVsTwoOutcome', groupvars='sex')
+bg2.vals.valid$Dataset <- rep('Validation', nrow(bg2.vals.valid))
+bg2.vals <- rbind(bg2.vals.train, bg2.vals.valid)
+bg2.vals$Dataset <- factor(bg2.vals$Dataset)
+bg2.vals$sex <- c('Male', 'Female', 'Male', 'Female')
+
+# Now lets plot our values
+# Grab a p value from a t.test
+pValue <- t.test(all.train.data$oneVsTwoOutcome ~ all.train.data$sex)
+pValue <- wilcox.test(all.train.data$oneVsTwoOutcome ~ all.train.data$sex)
+bg1 <- ggplot(bg2.vals[which(bg2.vals$Dataset=='Training'),], aes(x=factor(sex), y=as.numeric(as.character(oneVsTwoOutcome)), group=Dataset)) +
+                geom_bar(stat='identity', position=position_dodge(), width=.5) + 
+                labs(title='Training', x='Sex', y='Manual Quality Rating (mean value)') +
+                theme_bw() + 
+                coord_cartesian(ylim=c(.8,1)) +
+                       geom_errorbar(aes(ymin=as.numeric(as.character(oneVsTwoOutcome))-se, 
+                                         ymax=as.numeric(as.character(oneVsTwoOutcome))+se), 
+                width = .1, position=position_dodge(.9)) +
+                #facet_grid(Dataset ~ .) +
+                theme(legend.position="none",
+                axis.text=element_text(size=20),
+                axis.title=element_text(size=30),
+                strip.text.y = element_text(size = 16, angle = 270, face="bold"),
+                title=element_text(size=30)) + 
+		geom_path(aes(x=factor(sex), y=c(.95,.95))) +
+		geom_path(aes(x=factor(sex)[1], y=c(.95, .92))) +
+		geom_path(aes(x=factor(sex)[2], y=c(.94, .95))) +
+		geom_text(aes(x=factor(sex)[1], y=.97), label='*',angle=90, size=10) +
+		scale_y_continuous(limits=c(0, 1),
+                           breaks=round(seq(.8, 1, .2), digits=2), oob=rescale_none) +
+		annotate("text", x=c(Inf), y=c(Inf), label="p < 0.05", hjust=c(2.1), vjust=c(1.1), size=8, parse=T)
+		
+
+pValue <- t.test(all.valid.data$oneVsTwoOutcome ~ all.valid.data$sex)
+pValue <- wilcox.test(all.valid.data$oneVsTwoOutcome ~ all.valid.data$sex)
+bg2 <- ggplot(bg2.vals[which(bg2.vals$Dataset=='Validation'),], aes(x=factor(sex), y=as.numeric(as.character(oneVsTwoOutcome)), group=Dataset)) +
+                geom_bar(stat='identity', position=position_dodge(), width=.5) + 
+                labs(title='Validation', x='Sex', y='Manual Quality Rating (mean value)') +
+                theme_bw() +
+                coord_cartesian(ylim=c(.8,1)) +
+                       geom_errorbar(aes(ymin=as.numeric(as.character(oneVsTwoOutcome))-se, 
+                                         ymax=as.numeric(as.character(oneVsTwoOutcome))+se), 
+                width = .1, position=position_dodge(.9)) +
+                #facet_grid(Dataset ~ .) +
+                theme(legend.position="none",
+                axis.text.y=element_text(size=20, color='white'),
+                axis.title.y=element_text(size=30, color='white'),
+                axis.text=element_text(size=20),
+                axis.title=element_text(size=30),
+		axis.ticks.y=element_blank(),
+                strip.text.y = element_text(size = 16, angle = 270, face="bold"),
+                title=element_text(size=30)) + 
+		geom_path(aes(x=factor(sex), y=c(.95,.95))) +
+		geom_path(aes(x=factor(sex)[1], y=c(.95, .89))) +
+		geom_path(aes(x=factor(sex)[2], y=c(.94, .95))) +
+		geom_text(aes(x=factor(sex)[1], y=.97), label='***',angle=90, size=10) +
+		scale_y_continuous(limits=c(0, 1),
+                           breaks=round(seq(.8, 1, .2), digits=2), oob=rescale_none) +
+		annotate("text", x=c(Inf), y=c(Inf), label="p < 0.001", hjust=c(2.1), vjust=c(1.1), size=8, parse=T)
+
+
+# Now build our models to show geneerl age trends
+corVal <- cor(all.train.data$oneVsTwoOutcome, all.train.data$age, method='spearman')
+corSig <- cor.test(all.train.data$oneVsTwoOutcome, all.train.data$age, method='spearman')$p.value
+corText1 <- expression("Spearman's"~rho == .15)
+corText2 <- paste("p < 0.01")
+mod1 <- ggplot(all.train.data, aes(x=oneVsTwoOutcome, y=age)) +
+   geom_smooth(method=lm, color='black') +
+   theme_bw() +
+   coord_cartesian(ylim=c(7,16)) +
+   labs(title='', x='Manual Quality Rating (mean value)', y='Age') +
+   theme(
+    axis.text=element_text(size=20),
+    axis.title=element_text(size=30)) +
+   scale_x_continuous(breaks=c(0,.33,.66,1,1.33,1.66,2)) + 
+   annotate("text", x=c(Inf, Inf), y=c(-Inf, -Inf), label=c(as.character(corText2), as.character(corText1)), hjust=c(1, 1), vjust=c(-.5, -2.5), size=8, parse=T)
+
+
+corVal <- cor(all.valid.data$oneVsTwoOutcome, all.valid.data$age, method='spearman')
+corSig <- cor.test(all.valid.data$oneVsTwoOutcome, all.valid.data$age, method='spearman')$p.value
+corText1 <- expression("Spearman's"~rho == paste(0.31))
+corText2 <- paste("p < 0.01")
+mod2 <- ggplot(all.valid.data, aes(x=oneVsTwoOutcome, y=age)) +
+   geom_smooth(method=lm, color='black') +
+   theme_bw() +
+   coord_cartesian(ylim=c(7,16)) +
+   labs(title='', x='Manual Quality Rating (mean value)', y='Age') +
+   theme(
+    axis.text=element_text(size=20),
+    axis.title=element_text(size=30),
+    axis.title.y=element_text(size=20, color='white'),
+    axis.text.y=element_text(size=30, color='white'),
+    axis.ticks.y=element_blank()) +
+    scale_x_continuous(breaks=c(0,.33,.66,1,1.33,1.66,2)) + 
+   annotate("text", x=c(Inf, Inf), y=c(-Inf, -Inf), label=c(as.character(corText2), as.character(corText1)), hjust=c(1, 1), vjust=c(-.5, -2.5), size=8, parse=T)
+
+png('figure3-demographicsvs1vs2QAPPaper.png', width=16, height=16, units='in', res=300)
+multiplot(bg1, mod1, bg2, mod2, cols=2)
+dev.off()
+
+## Now do the same for the 0vs!0 model
+# Now prepare our values
+bg2.vals.train <- summarySE(data=all.train.data, measurevar='zeroVsNotZeroOutcome', groupvars='sex')
+bg2.vals.train$Dataset <- rep('Training', nrow(bg2.vals.train))
+bg2.vals.valid <- summarySE(data=all.valid.data, measurevar='zeroVsNotZeroOutcome', groupvars='sex')
+bg2.vals.valid$Dataset <- rep('Validation', nrow(bg2.vals.valid))
+bg2.vals <- rbind(bg2.vals.train, bg2.vals.valid)
+bg2.vals$Dataset <- factor(bg2.vals$Dataset)
+bg2.vals$sex <- c('Male', 'Female', 'Male', 'Female')
+
+# Now lets plot our values
+# Grab a p value from a t.test
+pValue <- t.test(all.train.data$zeroVsNotZeroOutcome ~ all.train.data$sex)
+pValue <- wilcox.test(all.train.data$zeroVsNotZeroOutcome ~ all.train.data$sex)
+bg1a <- ggplot(bg2.vals[which(bg2.vals$Dataset=='Training'),], aes(x=factor(sex), y=as.numeric(as.character(zeroVsNotZeroOutcome)), group=Dataset)) +
+geom_bar(stat='identity', position=position_dodge(), width=.5) +
+labs(title='Training', x='Sex', y='Manual Quality Rating (mean value)') +
+theme_bw() +
+coord_cartesian(ylim=c(.9,1.1)) +
+geom_errorbar(aes(ymin=as.numeric(as.character(zeroVsNotZeroOutcome))-se,
+ymax=as.numeric(as.character(zeroVsNotZeroOutcome))+se),
+width = .1, position=position_dodge(.9)) +
+#facet_grid(Dataset ~ .) +
+theme(legend.position="none",
+axis.text=element_text(size=20),
+axis.title=element_text(size=30),
+strip.text.y = element_text(size = 16, angle = 270, face="bold"),
+title=element_text(size=30)) +
+geom_path(aes(x=factor(sex), y=c(1.05,1.05))) +
+geom_path(aes(x=factor(sex)[1], y=c(1.05, .99))) +
+geom_path(aes(x=factor(sex)[2], y=c(1, 1.05))) +
+geom_text(aes(x=factor(sex)[1], y=1.07), label='***',angle=90, size=10) +
+scale_y_continuous(limits=c(0, 1),
+breaks=round(seq(.8, 1, .2), digits=2), oob=rescale_none) +
+annotate("text", x=c(Inf), y=c(Inf), label="p < 0.001", hjust=c(2.1), vjust=c(1.1), size=8, parse=T)
+
+
+pValue <- t.test(all.valid.data$zeroVsNotZeroOutcome ~ all.valid.data$sex)
+pValue <- wilcox.test(all.valid.data$zeroVsNotZeroOutcome ~ all.valid.data$sex)
+bg2a <- ggplot(bg2.vals[which(bg2.vals$Dataset=='Validation'),], aes(x=factor(sex), y=as.numeric(as.character(zeroVsNotZeroOutcome)), group=Dataset)) +
+geom_bar(stat='identity', position=position_dodge(), width=.5) +
+labs(title='Validation', x='Sex', y='Manual Quality Rating (mean value)') +
+theme_bw() +
+coord_cartesian(ylim=c(.9,1.1)) +
+geom_errorbar(aes(ymin=as.numeric(as.character(zeroVsNotZeroOutcome))-se,
+ymax=as.numeric(as.character(zeroVsNotZeroOutcome))+se),
+width = .1, position=position_dodge(.9)) +
+#facet_grid(Dataset ~ .) +
+theme(legend.position="none",
+axis.text.y=element_text(size=20, color='white'),
+axis.title.y=element_text(size=30, color='white'),
+axis.text=element_text(size=20),
+axis.title=element_text(size=30),
+axis.ticks.y=element_blank(),
+strip.text.y = element_text(size = 16, angle = 270, face="bold"),
+title=element_text(size=30)) +
+geom_path(aes(x=factor(sex), y=c(1.05,1.05))) +
+geom_path(aes(x=factor(sex)[1], y=c(1.05, .99))) +
+geom_path(aes(x=factor(sex)[2], y=c(1, 1.05))) +
+geom_text(aes(x=factor(sex)[1], y=1.07), label='***',angle=90, size=10) +
+scale_y_continuous(limits=c(0, 1),
+breaks=round(seq(.8, 1, .2), digits=2), oob=rescale_none) +
+annotate("text", x=c(Inf), y=c(Inf), label="p < 0.001", hjust=c(2.1), vjust=c(1.1), size=8, parse=T)
+
+
+# Now build our models to show geneerl age trends
+corVal <- cor(all.train.data$zeroVsNotZeroOutcome, all.train.data$age, method='spearman')
+corSig <- cor.test(all.train.data$zeroVsNotZeroOutcome, all.train.data$age, method='spearman')$p.value
+corText1 <- expression("Spearman's"~rho == .15)
+corText2 <- paste("p < 0.01")
+mod1a <- ggplot(all.train.data, aes(x=zeroVsNotZeroOutcome, y=age)) +
+geom_smooth(method=lm, color='black') +
+theme_bw() +
+coord_cartesian(ylim=c(5,20)) +
+labs(title='', x='Manual Quality Rating (mean value)', y='Age') +
+theme(
+axis.text=element_text(size=20),
+axis.title=element_text(size=30)) +
+scale_x_continuous(breaks=c(0,.25,.5,.75,1)) +
+annotate("text", x=c(Inf, Inf), y=c(-Inf, -Inf), label=c(as.character(corText2), as.character(corText1)), hjust=c(1, 1), vjust=c(-.5, -2.5), size=8, parse=T)
+
+
+corVal <- cor(all.valid.data$zeroVsNotZeroOutcome, all.valid.data$age, method='spearman')
+corSig <- cor.test(all.valid.data$zeroVsNotZeroOutcome, all.valid.data$age, method='spearman')$p.value
+corText1 <- expression("Spearman's"~rho == paste(0.31))
+corText2 <- paste("p < 0.01")
+mod2a <- ggplot(all.valid.data, aes(x=zeroVsNotZeroOutcome, y=age)) +
+geom_smooth(method=lm, color='black') +
+theme_bw() +
+coord_cartesian(ylim=c(5,20)) +
+labs(title='', x='Manual Quality Rating (mean value)', y='Age') +
+theme(
+axis.text=element_text(size=20),
+axis.title=element_text(size=30),
+axis.title.y=element_text(size=20, color='white'),
+axis.text.y=element_text(size=30, color='white'),
+axis.ticks.y=element_blank()) +
+scale_x_continuous(breaks=c(0,.25,.5,.75,1)) +
+annotate("text", x=c(Inf, Inf), y=c(-Inf, -Inf), label=c(as.character(corText2), as.character(corText1)), hjust=c(1, 1), vjust=c(-.5, -2.5), size=8, parse=T)
+
+
+png('figure3-demographicsvs0vsNot0QAPPaper.png', width=16, height=16, units='in', res=300)
+multiplot(bg1a, mod1a, bg2a, mod2a, cols=2)
+dev.off()
+png('figure3-allValues.png', width=16, height=32, units='in', res=300)
+multiplot(bg1, mod1, bg2, mod2, bg1a, mod1a, bg2a, mod2a, cols=2)
+dev.off()
+
+
+
+
+
+
+
+
+# Now build a lme model in the training data
+raw.lme.data <- merge(isolatedVars, manualQAData2, by='bblid')
+raw.lme.data <- melt(trainingData, id.vars = names(raw.lme.data)[1:32], measure.vars = names(raw.lme.data)[34:36])
+raw.lme.data$value[raw.lme.data$value > 1] <- 1
+train.data <- merge(raw.lme.data, all.train.data, by = "bblid")
+train.data$age <- as.numeric(as.character(train.data$age))
+m1 <- lmer(rawAverageRating.y ~ age + sex + (1|variable), data = train.data)
+sigValsTrain <- Anova(m1)
+
+# Now do the same for the validation data
+raw.lme.data <- merge(isolatedVars, manualQAData2, by='bblid')
+raw.lme.data <- melt(validationData, id.vars = names(raw.lme.data)[1:32], measure.vars = names(raw.lme.data)[34:36])
+raw.lme.data$value[raw.lme.data$value > 1] <- 1
+train.data <- merge(raw.lme.data, all.valid.data, by = "bblid")
+train.data$age <- as.numeric(as.character(train.data$age))
+m1 <- lmer(rawAverageRating.y ~ age + sex  + (1|variable), data = train.data)
+sigValsValid <- Anova(m1)
