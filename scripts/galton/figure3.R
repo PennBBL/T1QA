b87773ed5e55f1c20b040ce28c7372d076bdd854
Adon Rosen <arose@Adons-MacBook-Pro.local>
2017-06-24 22:17:45 -0400

    Added MGI to mean rating by sex and age figure

--- scripts/galton/figure3.R
+++ scripts/galton/figure3.R
@@ -6,6 +6,7 @@
 
 ## Load data
 # Start with Go1
+source('/home/adrose/T1QA/scripts/galton/loadMgiData.R')
 source('/home/adrose/T1QA/scripts/galton/loadGo1Data.R')
 set.seed(16)
 
@@ -38,10 +39,13 @@ bg1.vals$Dataset <- factor(bg1.vals$Dataset)
 bg2.vals.train <- summarySE(data=all.train.data, measurevar='averageRating', groupvars='sex')
 bg2.vals.train$Dataset <- rep('Training', nrow(bg2.vals.train))
 bg2.vals.valid <- summarySE(data=all.valid.data, measurevar='averageRating', groupvars='sex')
-bg2.vals.valid$Dataset <- rep('Validation', nrow(bg2.vals.valid))
-bg2.vals <- rbind(bg2.vals.train, bg2.vals.valid)
+bg2.vals.valid$Dataset <- rep('Testing', nrow(bg2.vals.valid))
+bg2.vals.mgi <- summarySE(data=all.mgi.data, measurevar='averageRating', groupvars='Gender')
+colnames(bg2.vals.mgi)[1] <- 'sex'
+bg2.vals.mgi$Dataset <- 'Validation'
+bg2.vals <- rbind(bg2.vals.train, bg2.vals.valid, bg2.vals.mgi)
 bg2.vals$Dataset <- factor(bg2.vals$Dataset)
-bg2.vals$sex <- c('Male', 'Female', 'Male', 'Female')
+bg2.vals$sex <- c('Male', 'Female', 'Male', 'Female', 'Male', 'Female')
 
 # Now lets plot our values
 # Grab a p value from a t.test
@@ -72,9 +76,9 @@ bg1 <- ggplot(bg2.vals[which(bg2.vals$Dataset=='Training'),], aes(x=factor(sex),
 
 pValue <- t.test(all.valid.data$averageRating ~ all.valid.data$sex)
 pValue <- wilcox.test(all.valid.data$averageRating ~ all.valid.data$sex)
-bg2 <- ggplot(bg2.vals[which(bg2.vals$Dataset=='Validation'),], aes(x=factor(sex), y=as.numeric(as.character(averageRating)), group=Dataset)) +
+bg2 <- ggplot(bg2.vals[which(bg2.vals$Dataset=='Training'),], aes(x=factor(sex), y=as.numeric(as.character(averageRating)), group=Dataset)) +
                 geom_bar(stat='identity', position=position_dodge(), width=.5) + 
-                labs(title='Validation', x='Sex', y='Manual Quality Rating (mean value)') +
+                labs(title='Training', x='Sex', y='Manual Quality Rating (mean value)') +
                 theme_bw() + 
                        geom_errorbar(aes(ymin=as.numeric(as.character(averageRating))-se, 
                                          ymax=as.numeric(as.character(averageRating))+se), 
@@ -95,7 +99,30 @@ bg2 <- ggplot(bg2.vals[which(bg2.vals$Dataset=='Validation'),], aes(x=factor(sex
 		scale_y_continuous(limits=c(1.6, 2.1), 
                            breaks=round(seq(1.6, 2.1, .2), digits=2), oob=rescale_none) + 
 		annotate("text", x=c(Inf), y=c(Inf), label="p < 0.001", hjust=c(2), vjust=c(1.1), size=8, parse=T)		
-
+pValue <- t.test(all.mgi.data$averageRating~ all.mgi.data$Gender)
+bg3 <- ggplot(bg2.vals[which(bg2.vals$Dataset=='Validation'),], aes(x=factor(sex), y=as.numeric(as.character(averageRating)), group=Dataset)) +
+geom_bar(stat='identity', position=position_dodge(), width=.5) +
+labs(title='Validation', x='Sex', y='Manual Quality Rating (mean value)') +
+theme_bw() +
+geom_errorbar(aes(ymin=as.numeric(as.character(averageRating))-se,
+ymax=as.numeric(as.character(averageRating))+se),
+width = .1, position=position_dodge(.9)) +
+#facet_grid(Dataset ~ .) +
+theme(legend.position="none",
+axis.text.y=element_text(size=20, color='white'),
+axis.title.y=element_text(size=30, color='white'),
+axis.text=element_text(size=20),
+axis.title=element_text(size=30),
+axis.ticks.y=element_blank(),
+strip.text.y = element_text(size = 16, angle = 270, face="bold"),
+title=element_text(size=30)) +
+geom_path(aes(x=factor(sex), y=c(2,2))) +
+geom_path(aes(x=factor(sex)[1], y=c(1.8, 2))) +
+geom_path(aes(x=factor(sex)[2], y=c(1.8, 2))) +
+geom_text(aes(x=factor(sex)[1], y=2.05), label='',angle=90, size=10) +
+scale_y_continuous(limits=c(1.6, 2.1),
+breaks=round(seq(1.6, 2.1, .2), digits=2), oob=rescale_none) +
+annotate("text", x=c(Inf), y=c(Inf), label="p > 0.9", hjust=c(2), vjust=c(1.1), size=8, parse=T)
 
 # Now build our models to show geneerl age trends
 corVal <- cor(all.train.data$rawAverageRating.x, all.train.data$age, method='spearman')
@@ -132,8 +159,27 @@ mod2 <- ggplot(all.valid.data, aes(x=rawAverageRating.x, y=age)) +
     scale_x_continuous(breaks=c(0,.33,.66,1,1.33,1.66,2)) + 
    annotate("text", x=c(Inf, Inf), y=c(-Inf, -Inf), label=c(as.character(corText2), as.character(corText1)), hjust=c(1, 1), vjust=c(-.5, -2.5), size=8, parse=T)
 
+corVal <- cor(all.mgi.data$rawAverageRating, all.mgi.data$age, method='spearman')
+corSig <- cor.test(all.mgi.data$rawAverageRating, all.mgi.data$age, method='spearman')$p.value
+corText1 <-expression("Spearman's"~rho == paste(-0.11))
+corText2 <- paste("p < 0.1")
+mod3 <- ggplot(all.mgi.data, aes(x=rawAverageRating, y=age)) +
+geom_smooth(method=lm, color='black') +
+theme_bw() +
+coord_cartesian(ylim=c(20,60)) +
+labs(title='', x='Manual Quality Rating (mean value)', y='Age') +
+theme(
+axis.text=element_text(size=20),
+axis.title=element_text(size=30),
+axis.title.y=element_text(size=20, color='white'),
+axis.text.y=element_text(size=30, color='black'),
+axis.ticks.y=element_blank()) +
+scale_x_continuous(breaks=c(0,.33,.66,1,1.33,1.66,2)) +
+annotate("text", x=c(Inf, Inf), y=c(-Inf, -Inf), label=c(as.character(corText2), as.character(corText1)), hjust=c(1, 1), vjust=c(-.5, -2.5), size=8, parse=T)
+
+
 png('figure3-demographicsvsRatingQAPPaper.png', width=16, height=16, units='in', res=300)
-multiplot(bg1, mod1, bg2, mod2, cols=2)
+multiplot(bg1, mod1, bg2, mod2, bg3, mod3, cols=3)
 dev.off()
 
 # Now build a lme model in the training data 
