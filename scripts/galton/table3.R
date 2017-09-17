09e403c3b3467a115bd29e07fee367db7558a5e4
Adon Rosen <adrose@galton.uphs.upenn.edu>
2017-09-16 21:30:53 -0400

    Added MGI  sample - reduced to only euler number

--- scripts/galton/table3.R
+++ scripts/galton/table3.R
@@ -4,6 +4,7 @@
 
 
 ## Load the data
+source("/home/adrose/T1QA/scripts/galton/loadMgiData.R")
 source('/home/adrose/T1QA/scripts/galton/loadGo1Data.R')
 set.seed(16)
 load('/home/adrose/qapQA/data/1vs28variableModel.RData')
@@ -13,98 +14,17 @@ rm(mod8)
 # Now load any library(s)
 install_load('ggplot2', 'caret', 'pROC')
 
-# Declare any functions
-rocdata <- function(grp, pred){
-    # Produces x and y co-ordinates for ROC curve plot
-    # Arguments: grp - labels classifying subject status
-    #            pred - values of each observation
-    # Output: List with 2 components:
-    #         roc = data.frame with x and y co-ordinates of plot
-    #         stats = data.frame containing: area under ROC curve, p value, upper and lower 95% confidence interval
-    
-    grp <- as.factor(grp)
-    if (length(pred) != length(grp)) {
-        stop("The number of classifiers must match the number of data points")
-    }
-    
-    if (length(levels(grp)) != 2) {
-        stop("There must only be 2 values for the classifier")
-    }
-    
-    cut <- unique(pred)
-    tp <- sapply(cut, function(x) length(which(pred > x & grp == levels(grp)[2])))
-    fn <- sapply(cut, function(x) length(which(pred < x & grp == levels(grp)[2])))
-    fp <- sapply(cut, function(x) length(which(pred > x & grp == levels(grp)[1])))
-    tn <- sapply(cut, function(x) length(which(pred < x & grp == levels(grp)[1])))
-    tpr <- tp / (tp + fn)
-    fpr <- fp / (fp + tn)
-    roc = data.frame(x = fpr, y = tpr)
-    roc <- roc[order(roc$x, roc$y),]
-    
-    i <- 2:nrow(roc)
-    auc <- (roc$x[i] - roc$x[i - 1]) %*% (roc$y[i] + roc$y[i - 1])/2
-    
-    pos <- pred[grp == levels(grp)[2]]
-    neg <- pred[grp == levels(grp)[1]]
-    q1 <- auc/(2-auc)
-    q2 <- (2*auc^2)/(1+auc)
-    se.auc <- sqrt(((auc * (1 - auc)) + ((length(pos) -1)*(q1 - auc^2)) + ((length(neg) -1)*(q2 - auc^2)))/(length(pos)*length(neg)))
-    ci.upper <- auc + (se.auc * 0.96)
-    ci.lower <- auc - (se.auc * 0.96)
-    
-    se.auc.null <- sqrt((1 + length(pos) + length(neg))/(12*length(pos)*length(neg)))
-    z <- (auc - 0.5)/se.auc.null
-    p <- 2*pnorm(-abs(z))
-    
-    stats <- data.frame (auc = auc,
-    p.value = p,
-    ci.upper = ci.upper,
-    ci.lower = ci.lower
-    )
-    
-    return (list(roc = roc, stats = stats))
-}
-
-
-rocplot.single <- function(grp, pred, title = "ROC Plot", p.value = FALSE){
-    require(ggplot2)
-    plotdata <- rocdata(grp, pred)
-    
-    if (p.value == TRUE){
-        annotation <- with(plotdata$stats, paste("AUC=",signif(auc, 2), " (P=", signif(p.value, 2), ")", sep=""))
-    } else {
-        annotation <- with(plotdata$stats, paste("AUC=",signif(auc, 2), " (95%CI ", signif(ci.upper, 2), " - ", signif(ci.lower, 2), ")", sep=""))
-    }
-    
-    p <- ggplot(plotdata$roc, aes(x = x, y = y)) +
-    geom_line(aes(colour = "")) +
-    geom_abline (intercept = 0, slope = 1) +
-    theme_bw() +
-    scale_x_continuous("False Positive Rate (1-Specificity)") +
-    scale_y_continuous("True Positive Rate (Sensitivity)") +
-    scale_colour_manual(labels = annotation, values = "#000000") +
-    ggtitle(title) +
-    theme_bw() +
-    theme(legend.position=c(1,0)) +
-    theme(legend.justification=c(1,0)) +
-    theme(legend.title=element_blank())
-    
-    return(p)
-}
-
 # Now load the models
 raw.lme.data <- merge(isolatedVars, manualQAData2, by='bblid')
 raw.lme.data$averageRating.x <- as.numeric(as.character(raw.lme.data$averageRating.x))
-raw.lme.data <- raw.lme.data[which(raw.lme.data$averageRating.x!=0),]
-raw.lme.data$averageRating.x[raw.lme.data$averageRating.x<1.5] <- 1
-raw.lme.data$averageRating.x[raw.lme.data$averageRating.x>1.5] <- 2
-folds <- createFolds(raw.lme.data$averageRating.x, k=3, list=T, returnTrain=T)
-raw.lme.data[,2:32] <- scale(raw.lme.data[,2:32], center=T, scale=T)
+raw.lme.data$averageRating.x[raw.lme.data$averageRating.x>1] <- 1
+#folds <- createFolds(raw.lme.data$averageRating.x, k=3, list=T, returnTrain=T)
+load('/home/adrose/qapQA/data/foldsToUse.RData')
+#raw.lme.data[,2:32] <- scale(raw.lme.data[,2:32], center=T, scale=T)
 index <- unlist(folds[1])
 raw.lme.data$value <- raw.lme.data$averageRating.x
 trainingData <- raw.lme.data[index,]
 validationData <- raw.lme.data[-index,]
-
 
 # Now prep our individual data sets
 all.train.data <- merge(trainingData, manualQAData, by='bblid')
@@ -112,26 +32,32 @@ all.valid.data <- merge(validationData, manualQAData, by='bblid')
 
 # Now create our train roc curve
 all.train.data$variable <- rep('ratingNULL', nrow(all.train.data))
-trainOutcome <- predict(oneVsTwoModel, newdata=all.train.data,
-allow.new.levels=T, type='response')
+trainOutcome <- all.train.data$mean_euler#predict(oneVsTwoModel, newdata=all.train.data,
+#allow.new.levels=T, type='response')
 trainValues <- all.train.data$averageRating.x
-roc.train <- roc(trainValues ~ trainOutcome, controls='1', cases='1')
+roc.train <- roc(trainValues ~ trainOutcome)
 
 # Now do our validation data
 all.valid.data$variable <- rep('ratingNULL', nrow(all.valid.data))
-validOutcome <- predict(oneVsTwoModel, newdata=all.valid.data,
-allow.new.levels=T, type='response')
+validOutcome <- all.valid.data$mean_euler#predict(oneVsTwoModel, newdata=all.valid.data,
+#allow.new.levels=T, type='response')
 validValues <- all.valid.data$averageRating.x
-roc.valid <- roc(validValues ~ validOutcome, controls='1', cases='1')
+roc.valid <- roc(validValues ~ validOutcome)
 
+# Now MGI data
+testOutcome <- all.mgi.data$mean_euler
+testValues <- all.mgi.data$averageRating
+testValues[which(testValues>1)] <- 1
+roc.test <- roc(testValues ~ testOutcome, controls=)
 
 # Now prepare our table's values
 output.train <- coords(roc.train, 'best')
 output.valid <- coords(roc.valid, output.train[1])
-
+output.test <- coords(roc.test, output.train[1])
 # Now create our table
-output <- rbind(output.train, output.valid)
-rownames(output) <- c('Train', 'Valid')
+
+output <- rbind(output.train, output.valid, output.test)
+rownames(output) <- c('Training', 'Internal Testing', 'External Testing')
 
 # Now write our csv
-write.csv(output, 'table3-oneVsTwoROCMetrics.csv', quote=F)
+write.csv(output, 'table3-useabilityROCMetrics.csv', quote=F)
