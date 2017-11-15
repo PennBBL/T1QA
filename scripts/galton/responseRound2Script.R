bea3312eefea27ad614838e840dc86203531af17
Adon Rosen <adrose@galton.uphs.upenn.edu>
2017-11-15 16:24:39 -0500

    Added empty script for response round 2

---
+++ scripts/galton/responseRound2Script.R
@@ -0,0 +1,7 @@
+# AFGR 
+
+# This script is going to be used to look at the overlap between the flagged freesurfer QA images and the ratings from the images 
+# WHat this is going to do is see where the flagged images came from and if any failed our QA procedure.
+
+# Load the data 
+source("/home/adrose/T1QA/scripts/galton/loadGo1Data.R")
