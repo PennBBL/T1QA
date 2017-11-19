ce83dfb4a7dc888e280a3de1e35f1f1cf7ad6ed6
Adon Rosen <arose@Adons-MacBook-Pro.local>
2017-11-18 23:06:11 -0500

    Began exploration for FS flag compare w/ average rating

--- scripts/galton/responseRound2Script.R
+++ scripts/galton/responseRound2Script.R
@@ -5,3 +5,11 @@
 
 # Load the data 
 source("/home/adrose/T1QA/scripts/galton/loadGo1Data.R")
+fsQAVals <- read.csv('~/n1601_t1QaData_20170306.csv')
+
+
+# Now I need to find the flagged images and where they came from
+mergedQAP <- merge(mergedQAP, fsQAVals)
+
+# NOw find a table with the flagged values
+flaggedValsTable <- table(mergedQAP$averageManualRating, mergedQAP$fsFlag)
